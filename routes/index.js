22e05dc718ff9a1e3662e46e21cdc8c24ae3245a
Mike James <m.l.james@me.com>
2016-07-31 12:46:21 +0100

    init

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* POST maker. */
+router.post('/maker', function(req, res, next) {
+  console.log('/maker', req.body);
+  res.render('index', { title: 'Maker' });
+});
+
 module.exports = router;
