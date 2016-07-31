646e6ff51d70ab6cdf346ccd863503bad0dd6b54
Mike James <m.l.james@me.com>
2016-07-31 20:20:52 +0100

    force ssl

--- routes/index.js
+++ routes/index.js
@@ -18,21 +18,25 @@ router.get('/log', (req, res) => {
 
 router.get('/auth/dropbox', (req, res) => {
   debug('querystrings', req.query)
-
+  const protocol = process.env.NODE_ENV === 'development' ? 'http' : 'https';
   if (!req.query.code) {
-    dropbox.Authenticate(process.env.DROPBOX_KEY, process.env.DROPBOX_SECRET, `${req.protocol}://${req.get('host')}/auth/dropbox`, (err, url) => {
-      if (err || !url) {
-        return res.status(500).send('Error generating url for dropbox');
-      }
-      debug('url1', url);
-      res.redirect(307, url);
+    dropbox.Authenticate(
+      process.env.DROPBOX_KEY,
+      process.env.DROPBOX_SECRET,
+      `${protocol}://${req.get('host')}/auth/dropbox`,
+      (err, url) => {
+        if (err || !url) {
+          return res.status(500).send('Error generating url for dropbox');
+        }
+        debug('url1', url);
+        res.redirect(307, url);
     });
   } else if (req.query.code) {
     dropbox.AccessToken(
       process.env.DROPBOX_KEY,
       process.env.DROPBOX_SECRET,
       req.query.code,
-      `${req.protocol}://${req.get('host')}/auth/dropbox`,
+      `${protocol}://${req.get('host')}/auth/dropbox`,
       (err, account) => {
         if (err) {
           return res.status(500).send('Error Authenticating');
